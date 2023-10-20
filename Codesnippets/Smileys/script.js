fb0f56e04c1d29105725c711a25b02de992d5b91
Martin Schneider <schnmart86@gmail.com>
2023-10-20 11:36:14 +0200

    feat: add snpppet Section

---
+++ Codesnippets/Smileys/script.js
@@ -0,0 +1,7 @@
+//UTF-16
+//  0xD83D 0xDE00 to \uD83D\uDE00
+console - console.log("\uD83D\uDE00");
+
+//Unicode Hexadeciaml
+//  U+1F600 to 0x1f600
+console - console.log(String.fromCodePoint(0x1f600));
