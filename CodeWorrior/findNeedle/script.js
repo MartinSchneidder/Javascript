6189449f179875d436049322ee09017f516e19f1
Martin Schneider <schnmart86@gmail.com>
2023-10-24 14:54:13 +0200

    add: Task: find index of element

---
+++ CodeWorrior/findNeedle/script.js
@@ -0,0 +1,17 @@
+/**
+ * Finds the index of an element in an array
+ * @param {Array} haystack
+ */
+function findNeedle(haystack) {
+  let pos = 0;
+  for (let index = 0; index < haystack.length; index++) {
+    if (haystack[index] !== "needle") {
+      pos++;
+    } else break;
+  }
+  return "found the needle at position " + pos;
+  //return "found the needle at position " + haystack.indexOf("needle");
+}
+
+let arr = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
+console.log(findNeedle(arr));
